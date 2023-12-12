7352ab42402572ed36bafba0597b672dfddf4c82
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2023-12-12 16:22:25 +0530

    this is 7th commit

--- Shell-Scripts/08-array.sh
+++ Shell-Scripts/08-array.sh
@@ -2,6 +2,12 @@
 
 fruits=("Mango" "Banana" "Apple" "Orange")
 
-echo "First value: ${fruits[0]}"
+echo "1st value: ${fruits[0]}"
 
-echo "Second value: $fruits[1]"
+echo "2nd value: ${fruits[1]}"
+
+echo "3rd value: ${fruits[2]}"
+
+echo "4th value: ${fruits[0]}"
+
+echo "all value: ${fruits[@]}"
