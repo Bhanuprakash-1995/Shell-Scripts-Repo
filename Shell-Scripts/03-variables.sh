a37145b531bb8a523aeb9d868230c646eb3993f0
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2023-12-13 15:55:25 +0530

    This 1st commit for today

--- Shell-Scripts/03-variables.sh
+++ Shell-Scripts/03-variables.sh
@@ -4,3 +4,10 @@ USER2=$2
 
 echo "$1 :: Hi $2, Good Morning"
 echo "$2 :: Hello $1 Good Morning"
+
+echo "To Know the exit status:: $?"
+echo "To know the script name:: $0"
+echo "To Know the 1st argument:: $1"
+echo "To Know the 2nd argument:: $2"
+echo "To Know the how many arguments passed to the scripts:: $#"
+echo "To know the all the argument:: $@"
