e662877e5a6e8bd5fb3751941b42df98c5e90d6c
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2023-12-15 13:34:04 +0530

    Friday 2nd

--- Shell-Scripts/Robo-Shell/cart.sh
+++ Shell-Scripts/Robo-Shell/cart.sh
@@ -17,7 +17,7 @@ N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "Script started executing at $TIMESTAMP" &>>"$LOGFILE"
+echo "Script started executing at $TIMESTAMP" &>>$LOGFILE
 
 if [ "$ID" -ne 0 ]; then
     echo -e "$R ERROR:: Please run this script with root access $N"
@@ -34,21 +34,20 @@ VALIDATE() {
     fi
 }
 
-dnf module disable nodejs -y &>>"$LOGFILE"
+dnf module disable nodejs -y &>>$LOGFILE
 
 VALIDATE $? "Disabling current NodeJS"
 
-dnf module enable nodejs:18 -y &>>"$LOGFILE"
+dnf module enable nodejs:18 -y &>>$LOGFILE
 
 VALIDATE $? "Enabling NodeJS:18"
 
-dnf install nodejs -y &>>"$LOGFILE"
+dnf install nodejs -y &>>$LOGFILE
 
 VALIDATE $? "Installing NodeJS:18"
 
 id roboshop
 
-# shellcheck disable=SC2181
 if [ $? -ne 0 ]; then
     useradd roboshop
     VALIDATE $? "roboshop user creation"
@@ -60,30 +59,28 @@ mkdir -p /app
 
 VALIDATE $? "creating app directory"
 
-curl -L -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>>"$LOGFILE"
+curl -L -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>>$LOGFILE
 
 VALIDATE $? "Downloading cart application"
 
 cd /app
 
-unzip -o /tmp/cart.zip &>>"$LOGFILE"
+unzip -o /tmp/cart.zip
 
-VALIDATE $? "unzipping cart"
+VALIDATE $? "unzipping cart" &>>$LOGFILE
 
-npm install &>>"$LOGFILE"
+npm install
 
-cp /home/centos/Shell-Scripts-Repo/Shell-Scripts/Robo-Shell/cart.service /etc/systemd/system/cart.service &>>"$LOGFILE"
-
-VALIDATE $? "Cart Service"
+cp /home/centos/Shell-Scripts-Repo/Shell-Scripts/Robo-Shell/cart.service /etc/systemd/system/cart.service
 
 systemctl daemon-reload
 
-VALIDATE $? "Daemon-reload cart Service"
+VALIDATE $? "Daemon-reload cart Service" &>>$LOGFILE
 
 systemctl enable cart
 
-VALIDATE $? "Enable cart Service"
+VALIDATE $? "Enable cart Service" &>>$LOGFILE
 
 systemctl start cart
 
-VALIDATE $? "Start cart Service"
+VALIDATE $? "Start cart Service" &>>$LOGFILE
