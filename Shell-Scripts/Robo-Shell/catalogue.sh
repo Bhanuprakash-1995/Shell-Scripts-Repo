a67743aed3123f1d2d0454caaf57db3864646184
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2023-12-15 12:42:46 +0530

    Friday 2nd

--- Shell-Scripts/Robo-Shell/catalogue.sh
+++ Shell-Scripts/Robo-Shell/catalogue.sh
@@ -17,7 +17,7 @@ N="\e[0m"
 MONGDB_HOST=mongodb.roboshopapp.website
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "Script started executing at $TIMESTAMP" &>>"$LOGFILE"
+echo "Script started executing at $TIMESTAMP" &>>$LOGFILE
 
 if [ "$ID" -ne 0 ]; then
     echo -e "$R ERROR:: Please run this script with root access $N"
@@ -36,67 +36,67 @@ VALIDATE() {
 
 dnf module disable nodejs -y
 
-VALIDATE $? "Disabling current NodeJS" &>>"$LOGFILE"
+VALIDATE $? "Disabling current NodeJS" &>>$LOGFILE
 
 dnf module enable nodejs:18 -y
 
-VALIDATE $? "Enabling NodeJS:18" &>>"$LOGFILE"
+VALIDATE $? "Enabling NodeJS:18" &>>$LOGFILE
 
 dnf install nodejs -y
 
-VALIDATE $? "Installing NodeJS:18" &>>"$LOGFILE"
+VALIDATE $? "Installing NodeJS:18" &>>$LOGFILE
 
 id roboshop
 
 if [ $? -ne 0 ]; then
     useradd roboshop
-    VALIDATE $? "roboshop user creation" &>>"$LOGFILE"
+    VALIDATE $? "roboshop user creation" &>>$LOGFILE
 else
-    echo -e "roboshop user already exist $Y SKIPPING $N" &>>"$LOGFILE"
+    echo -e "roboshop user already exist $Y SKIPPING $N" &>>$LOGFILE
 fi
 
 mkdir -p /app
 
-VALIDATE $? "creating app directory" &>>"$LOGFILE"
+VALIDATE $? "creating app directory" &>>$LOGFILE
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>"$LOGFILE"
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
 
-VALIDATE $? "Downloading catalogue application" &>>"$LOGFILE"
+VALIDATE $? "Downloading catalogue application" &>>$LOGFILE
 
-cd /app || exit
+cd /app
 
 unzip -o /tmp/catalogue.zip
 
-VALIDATE $? "unzipping catalogue" &>>"$LOGFILE"
+VALIDATE $? "unzipping catalogue" &>>$LOGFILE
 
 npm install
 
-VALIDATE $? "Installing dependencies"
+VALIDATE $? "Installing dependencies" &>>$LOGFILE
 
-cp /home/centos/Shell-Scripts-Repo/Shell-Scripts/Robo-Shell/catalogue.service /etc/systemd/system/catalogue.service &>>"$LOGFILE"
+cp /home/centos/Shell-Scripts-Repo/Shell-Scripts/Robo-Shell/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
 
-VALIDATE $? "Copying catalogue service file"
+VALIDATE $? "Copying catalogue service file" &>>$LOGFILE
 
 systemctl daemon-reload
 
-VALIDATE $? "catalogue daemon reload" &>>"$LOGFILE"
+VALIDATE $? "catalogue daemon reload" &>>$LOGFILE
 
 systemctl enable catalogue
 
-VALIDATE $? "Enable catalogue" &>>"$LOGFILE"
+VALIDATE $? "Enable catalogue" &>>$LOGFILE
 
 systemctl start catalogue
 
-VALIDATE $? "Starting catalogue" &>>"$LOGFILE"
+VALIDATE $? "Starting catalogue" &>>$LOGFILE
 
 cp /home/centos/Shell-Scripts-Repo/Shell-Scripts/Robo-Shell/mongo.repo /etc/yum.repos.d/mongo.repo
 
-VALIDATE $? "copying mongodb repo" &>>"$LOGFILE"
+VALIDATE $? "copying mongodb repo" &>>$LOGFILE
 
 dnf install mongodb-org-shell -y
 
-VALIDATE $? "Installing MongoDB client" &>>"$LOGFILE"
+VALIDATE $? "Installing MongoDB client" &>>$LOGFILE
 
 mongo --host $MONGDB_HOST </app/schema/catalogue.js
 
-VALIDATE $? "Loading catalouge data into MongoDB" &>>"$LOGFILE"
+VALIDATE $? "Loading catalouge data into MongoDB" &>>$LOGFILE
