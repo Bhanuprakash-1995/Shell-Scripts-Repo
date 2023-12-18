df4363ea8c7782d8adca49dcf830fbec4ed43ded
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2023-12-18 20:14:27 +0530

    shell commit

--- Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/01-assign.sh
+++ Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/01-assign.sh
@@ -2,9 +2,9 @@
 SOURCE_DIR=$1
 ACTION=$2
 DEFAULT_DAYS_TO_DELETE=14
-# DEFAULT_DAYS_TO_ARCHIVE=14
+DEFAULT_DAYS_TO_ARCHIVE=14
 DAYS_TO_DELETE_ARCHIVE=${3:-$DEFAULT_DAYS_TO_DELETE}
-# DESTINATION=$4
+DESTINATION=$4
 
 if [ -d "$SOURCE_DIR" ] && [ "$ACTION" = "delete" ]; then
     FILES_TO_FIND=$(find $SOURCE_DIR -type f -mtime +$DAYS_TO_DELETE_ARCHIVE -name "*.log")
@@ -20,14 +20,7 @@ else
     exit 1
 fi
 
-# if [ -d "$SOURCE_DIR" ] && [ "$ACTION" = "archive" ]; then
-#     mkdir -p $DESTINATION
-#     FILES_TO_FIND=$(find $SOURCE_DIR -type f -mtime +$DAYS_TO_DELETE -name "*.log")
-#     FILE_COUNT=$(echo "$FILES_TO_FIND" | wc -l)
-#     if [ "$FILE_COUNT" -gt 0 ]; then
-#         while IFS= read -r line; do
-#             zip "$DESTINATION.archive.zip" $line
-#         done <<<$FILES_TO_FIND
-#     fi
+if [ -d "$SOURCE_DIR" ] && [ "$ACTION" = "archive" ]; then
+    mkdir -p $DESTINATION
 
-# fi
+fi
