9fac371a11aca8c545f8fa03c2f6dbaeaba58b71
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2023-12-18 16:16:29 +0530

    shell commit

--- Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/assignment-delete-archive-file.sh
+++ Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/assignment-delete-archive-file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SOURCE_DIR=$1
-
+OPTION=$2
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -13,16 +13,20 @@ else
     echo -e "$SOURCE_DIR exsits"
 fi
 
-FILES_TO_FIND=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
-
-if [ ! $FILES_TO_FIND ]; then
-    echo "There no files to delete"
-    exit 1
-else
-    echo "List of filet to delete: $FILES_TO_FIND"
+if [ $OPTION == "delete" ]; then
+    echo "User Chosed to delete the files"
 fi
 
-while IFS= read -r line; do
-    echo -e "Deleting file: $line"
-    rm -rf $line
-done <<<$FILES_TO_FIND
+# FILES_TO_FIND=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+
+# if [ ! $FILES_TO_FIND ]; then
+#     echo "There no files to delete"
+#     exit 1
+# else
+#     echo "List of filet to delete: $FILES_TO_FIND"
+# fi
+
+# while IFS= read -r line; do
+#     echo -e "Deleting file: $line"
+#     rm -rf $line
+# done <<<$FILES_TO_FIND
