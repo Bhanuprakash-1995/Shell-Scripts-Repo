924438f1d4a3e76dccc9dbc070491acac178baa0
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2023-12-18 17:24:58 +0530

    shell commit

--- Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/assignment-1.sh
+++ Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/assignment-1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SOURCE_DIR=$1
 ACTION=$2
+DEFAULT_DAYS_TO_DELETE=14
 DAYS_TO_DELETE=${1:-$DEFAULT_DAYS_TO_DELETE}
 # echo "$@ these are the arguments passed"
 
@@ -15,6 +16,10 @@ if [ ! -d $SOURCE_DIR ]; then
 else
     echo -e "$SOURCE_DIR exsits"
 fi
+# if ! [[ $DAYS_TO_DELETE =~ ^[0-9]+$ ]]; then
+#     echo "Invalid or no numeric value provided. Using default value: $DEFAULT_DAYS_TO_DELETE"
+#     DAYS_TO_DELETE=$DEFAULT_DAYS_TO_DELETE
+# fi
 
 if [ "$ACTION" == "delete" ]; then
     FILES_TO_DELETE=$(find "$SOURCE_DIR" -type f -mtime +$DAYS_TO_DELETE -name "*.log")
