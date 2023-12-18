67972be4edb315b5651f58f91f456268e81d0a9d
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2023-12-18 18:36:22 +0530

    shell commit

--- Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/assignment-1.sh
+++ Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/assignment-1.sh
@@ -16,7 +16,7 @@ else
     echo -e "$SOURCE_DIR exsits"
 fi
 
-if [ $ACTION == "delete" ]; then
+if [ "$ACTION" == "delete" ]; then
     FILES_TO_DELETE=$(find "$SOURCE_DIR" -type f -name "*.log" -mtime +$DAYS_TO_DELETE)
     if [ $FILES_TO_DELETE -gt 0 ]; then
         echo "User chose to delete the files older than $DAYS_TO_DELETE days"
