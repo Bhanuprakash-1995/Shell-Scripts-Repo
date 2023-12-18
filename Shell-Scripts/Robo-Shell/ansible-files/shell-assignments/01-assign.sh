b16376d9cdbe0964d9cf38c0bcb772f0fb2c3d4d
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2023-12-18 20:03:12 +0530

    shell commit

--- Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/01-assign.sh
+++ Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/01-assign.sh
@@ -2,11 +2,12 @@
 SOURCE_DIR=$1
 ACTION=$2
 DEFAULT_DAYS_TO_DELETE=14
-DAYS_TO_DELETE_ORARCHIVE=${3:-$DEFAULT_DAYS_TO_DELETE}
-DESTINATION=$5
+DEFAULT_DAYS_TO_ARCHIVE=14
+DAYS_TO_DELETE_ARCHIVE=${3:-$DEFAULT_DAYS_TO_DELETE}
+DESTINATION=$4
 
 if [ -d "$SOURCE_DIR" ] && [ "$ACTION" = "delete" ]; then
-    FILES_TO_FIND=$(find $SOURCE_DIR -type f -mtime +$DAYS_TO_DELETE_ORARCHIVE -name "*.log")
+    FILES_TO_FIND=$(find $SOURCE_DIR -type f -mtime +$DAYS_TO_DELETE_ARCHIVE -name "*.log")
     FILE_COUNT=$(echo "$FILES_TO_FIND" | wc -l)
     if [ "$FILE_COUNT" -gt 0 ]; then
         while IFS= read -r line; do
@@ -20,12 +21,13 @@ else
 fi
 
 if [ -d "$SOURCE_DIR" ] && [ "$ACTION" = "archive" ]; then
-    mkdir -d "$DESTINATION"
-    FILES_TO_FIND=$(find $SOURCE_DIR -type f -mtime +$DAYS_TO_DELETE_ORARCHIVE -name "*.log")
+    mkdir -d $DESTINATION
+    FILES_TO_FIND=$(find $SOURCE_DIR -type f -mtime +$DAYS_TO_DELETE -name "*.log")
     FILE_COUNT=$(echo "$FILES_TO_FIND" | wc -l)
     if [ "$FILE_COUNT" -gt 0 ]; then
         while IFS= read -r line; do
-            tar -czvf "$DESTINATION" $line
+            zip "$DESTINATION.archive.zip" $line
         done <<<$FILES_TO_FIND
     fi
+
 fi
