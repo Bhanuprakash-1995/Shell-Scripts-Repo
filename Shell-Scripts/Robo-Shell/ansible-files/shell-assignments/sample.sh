e650a21c63d066d526f410db68d0d87e4b6bb837
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2023-12-18 17:45:06 +0530

    shell commit

--- Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/sample.sh
+++ Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/sample.sh
@@ -2,5 +2,6 @@
 DEFAULT_DAYS_TO_DELETE=14
 DAYS_TO_DELETE=${2:-$DEFAULT_DAYS_TO_DELETE}
 SOURCE_DIR=$1
+echo "$DAYS_TO_DELETE ...!"
 FILES_TO_DELETE=$(find "$SOURCE_DIR" -type f -name "*.log" -mtime +"$DAYS_TO_DELETE")
 echo "$FILES_TO_DELETE files to delete..!"
