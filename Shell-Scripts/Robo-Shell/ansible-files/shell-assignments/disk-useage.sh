1cc69a66702824222ccd1a26942aaebe57ff3abb
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2023-12-18 15:00:51 +0530

    shell commit

--- Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/disk-useage.sh
+++ Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/disk-useage.sh
@@ -8,10 +8,12 @@ while IFS= read line; do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
     partition=$(echo $line | awk '{print $1F}')
     if [ $usage -ge $DISK_THRESHOLD ]; then
-        message+="High disk usage on $partition: $usage\n"
+        message+="High disk usage on $partition: $usage <br>"
     fi
 done <<<$DISK_USAGE
 
 echo -e "Message: $message"
 
-echo "$message" | mail -s "Hisk Disk Usage" bhanupadhu21071995@gmail.com
+# echo "$message" | mail -s "Hisk Disk Usage" bhanupadhu21071995@gmail.com
+
+sh mail.sh "Devops Team" "High Disk Usage" "$message" "bhanupadhu21071995@gmail.com" "ALERT High Disk Usage"
