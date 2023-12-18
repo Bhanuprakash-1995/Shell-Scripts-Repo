6623de02fc5e5b1169882c32cd205a37c9117212
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2023-12-18 17:28:11 +0530

    shell commit

--- Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/assignment-1.sh
+++ Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/assignment-1.sh
@@ -16,13 +16,16 @@ if [ ! -d $SOURCE_DIR ]; then
 else
     echo -e "$SOURCE_DIR exsits"
 fi
-# if ! [[ $DAYS_TO_DELETE =~ ^[0-9]+$ ]]; then
-#     echo "Invalid or no numeric value provided. Using default value: $DEFAULT_DAYS_TO_DELETE"
-#     DAYS_TO_DELETE=$DEFAULT_DAYS_TO_DELETE
-# fi
+if ! [[ $DAYS_TO_DELETE =~ ^[0-9]+$ ]]; then
+    echo "Invalid or no numeric value provided. Using default value: $DEFAULT_DAYS_TO_DELETE"
+    DAYS_TO_DELETE=$DEFAULT_DAYS_TO_DELETE
+fi
 
+# Rest of your script
+
+# Use DAYS_TO_DELETE in your file deletion operation
 if [ "$ACTION" == "delete" ]; then
-    FILES_TO_DELETE=$(find "$SOURCE_DIR" -type f -mtime +"$DAYS_TO_DELETE" -name "*.log")
+    FILES_TO_DELETE=$(find "$SOURCE_DIR" -type f -name "*.log" -mtime +"$DAYS_TO_DELETE")
     echo "User chose to delete the files older than $DAYS_TO_DELETE days"
     if [ -n "$FILES_TO_DELETE" ]; then
         while IFS= read -r line; do
