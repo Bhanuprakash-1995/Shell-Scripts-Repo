f9ba35d54eb934eb6784771cebaeae4fb0019ba8
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2023-12-18 12:01:19 +0530

    shell commit

--- Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/delete-old-file.sh
+++ Shell-Scripts/Robo-Shell/ansible-files/shell-assignments/delete-old-file.sh
@@ -2,11 +2,21 @@
 
 SOURCE_DIR="/tmp/shell-scripts"
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 if [ ! -d $SOURCE_DIR ]; then
-    echo -e "$SOURCE_DIR Does not exsits"
+    echo -e "$R $SOURCE_DIR Does not exsits"
 else
     echo -e "$SOURCE_DIR exsits"
 fi
 
 FILES_TO_FIND=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 echo "List of filet to delete: $FILES_TO_FIND"
+
+while IFS= read -r line; do
+    echo -e "Deleting file: $line"
+    rm -rf $line
+done <<<$FILES_TO_FIND
